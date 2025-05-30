a2c4a2ff5708f03f2a606b1e5d3483a93a6f3b03
God-Children <azaeldieuchildrensawadogo@gmail.com>
2025-05-30 21:22:27 +0100

    Update config.js

--- config.js
+++ config.js
@@ -19,7 +19,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "PUT YOUR SESSION ID HERE",
+SESSION_ID: process.env.SESSION_ID || "GoJo-MD~ZzlhBIbY#Px_fRHPXGP9607X84QJZdLL0jdzwG8R8yh5TBwwTfng",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 MODE: process.env.MODE || "private",
 AUTO_VOICE: process.env.AUTO_VOICE || "false",
